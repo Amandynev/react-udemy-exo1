ddc671ad374e7f0fe862d772754b3d459df22dc3
Amandyne Verdonck <amandyne.verdonck@gmail.com>
2021-01-22 17:12:52 +0100

    input formulaire

--- src/App.js
+++ src/App.js
@@ -42,15 +42,27 @@ class App extends Component {
     this.setState({ famille })
   }
 
+  handleChange = (event) => {
+    // d'abord on doit copier/dire le state à changer
+    const famille = { ...this.state.famille }
+    // on applicuqe le changement quand on click
+    famille.member1.name = event.target.value
+
+    //  on modifie le state
+    this.setState({ famille })
+  }
+
 render () {
+    const { famille } = this.state
     return (
       <div className='App'>
         <h2>Bienvenue dans la famille de Did !</h2>
-        <Membre name={this.state.famille.member1.name} age= {this.state.famille.member1.age}/>
-        <Membre name={this.state.famille.member2.name} age= {this.state.famille.member2.age}/>
-        <Membre name={this.state.famille.member3.name} age= {this.state.famille.member3.age}/>
-        <Membre name={this.state.famille.member4.name} age= {this.state.famille.member4.age}/>
-        <Membre name={this.state.famille.member5.name} age= {this.state.famille.member5.age}/>
+        <input value= {this.state.famille.member1.name} onChange={this.handleChange} type="text"/>
+        <Membre name={famille.member1.name} age= {famille.member1.age}/>
+        <Membre name={famille.member2.name} age= {famille.member2.age}/>
+        <Membre name={famille.member3.name} age= {famille.member3.age}/>
+        <Membre name={famille.member4.name} age= {famille.member4.age}/>
+        <Membre name={famille.member5.name} age= {famille.member5.age}/>
         <Button older={ () => this.handleClick(5) }/>
       </div>
       )
